e56ed718fb80a7347334fc99b8e3f51a754a9074
StepfenShawn <m18824909883@163.com>
2022-12-22 11:38:05 +0800

    Add a blogs list  page

--- app.py
+++ app.py
@@ -80,12 +80,25 @@ async def response_factory(app, handler):
     return resp
   return response
 
+def datetime_filter(t):
+  delta = int(time.time() - t)
+  if delta < 60:
+    return "1分钟前"
+  if delta < 3600:
+    return '%s分钟前' % (delta // 60)
+  if delta < 86400:
+    return '%s小时前' % (delta // 3600)
+  if delta < 604800:
+    return '%s天前' % (delta // 86400)
+  dt = datetime.fromtimestamp(t)
+  return '%s年%s月%s日' % (dt.year, dt.month, dt.day)
+
 async def init(loop):
   await orm.create_pool(loop=loop, host='127.0.0.1', port=3306, user='root', password='root', db='awesome')
   app = web.Application(middlewares=[
     logger_factory ,response_factory
   ])
-  init_jinja2(app)
+  init_jinja2(app, filters = dict(datetime = datetime_filter))
   add_route(app, index)
   add_static(app)
 
