8a8855b9a663256ef6c06d79076e9526bd038cd1
StepfenShawn <m18824909883@163.com>
2022-12-26 15:06:44 +0800

    Fix bugs in orm

--- handle.py
+++ handle.py
@@ -58,12 +58,7 @@ async def cookie2user(cookie_str):
 
 @get('/')
 async def index(request):
-  summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-  blogs = [
-    Blog(id='1', name='Test Blog', summary=summary, created_at=time.time() - 120),
-    Blog(id='2', name='Something New', summary=summary, created_at=time.time() - 3600),
-    Blog(id='3', name='Learn Swift', summary=summary, created_at=time.time() - 7200)
-  ]
+  blogs = await Blog.findAll()
   return {
     '__template__': 'blogs.html',
     'blogs': blogs,
@@ -77,11 +72,15 @@ async def get_blog(id):
   for c in comments:
     c.html_content = text2html(c.content)
   blog.html_content = markdown2.markdown(blog.content)
+  # TODO: add a blog view page
+  """
   return {
     '__template__' : 'blog.html',
     'blog' : blog,
     'comments' : comments
   }
+  """
+  return blog.html_content
 
 @get('/api/users')
 async def api_get_users():
