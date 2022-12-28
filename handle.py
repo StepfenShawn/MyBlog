5bedc5ffe42351913bac6d25b0f32794939812a6
StepfenShawn <m18824909883@163.com>
2022-12-28 16:22:36 +0800

    I think we need to use location.reload() to refresh our web page

--- handle.py
+++ handle.py
@@ -224,5 +224,5 @@ async def api_delate_blog(request, *, id):
 handler_list = [index, api_get_users, register, signin, 
                 api_register_user, authenticate, api_create_blog,
                 manage_create_blog, get_blog, manage_blogs, api_blogs,
-                api_delate_blog, api_create_comment
+                api_delate_blog, api_create_comment, api_get_blog
               ]
