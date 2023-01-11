83e372681809d80c20fd4f4bf00fab5ea77290d3
StepfenShawn <m18824909883@163.com>
2023-01-11 13:52:52 +0800

    Fix bug in search page

--- handles/search.py
+++ handles/search.py
@@ -1,5 +1,5 @@
 from coroweb import get, post
-from models import Blog
+from models import Blog, User, Comment
 from aiohttp import web_request
 
 @get('/api/search/{content}')
@@ -12,9 +12,14 @@ async def api_search_content(content, request : web_request.Request):
 
 @get('/search/{content}')
 async def search_content(content, request : web_request.Request):
-  rs = await Blog.find_use_like('content', content)
+  blog_rs = await Blog.find_use_like('content', content)
+  user_rs = await User.find_use_like('name', content)
+  comment_rs = await Comment.find_use_like('content', content)
   return {
-    'result_list' : rs,
+    'blog_rs' : blog_rs,
+    'user_rs' : user_rs,
+    'comment_rs' : comment_rs,
     '__template__' : "search_result.html",
-    '__user__' : request.__user__
+    '__user__' : request.__user__,
+    'content' : content
   }
